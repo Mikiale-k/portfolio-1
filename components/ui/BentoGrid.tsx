0eb33f985fefd4941b1f75902dd1939f52a4e55d
Mikiale-K <mikialekesate>
2024-11-25 19:38:54 +0300

    adding new ul components

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,11 +1,15 @@
+"use client";
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
-
-
+import Lottie from "react-lottie";
 
 import { cn } from "@/lib/utils";
 
+import { BackgroundGradientAnimation } from "./GradientBg";
+import GridGlobe from "./GridGlobe";
+import animationData from "@/data/confetti.json";
+import MagicButton from "./MagicButton";
 
 export const BentoGrid = ({
   className,
@@ -17,6 +21,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
+        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
         "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
@@ -31,6 +36,7 @@ export const BentoGridItem = ({
   id,
   title,
   description,
+  //   remove unecessary things here
   img,
   imgClassName,
   titleClassName,
@@ -60,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "mikialekesate@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
@@ -72,13 +78,11 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        
         background: "rgb(4,7,29)",
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-   
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
@@ -114,25 +118,20 @@ export const BentoGridItem = ({
             "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
-          
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
-           , remove text-neutral-600 dark:text-neutral-300*/}
-         
+
           <div
             className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
           >
             {title}
           </div>
 
-          
           {id === 2 && <GridGlobe />}
 
-         
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-             
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                 {leftLists.map((item, i) => (
                   <span
@@ -161,13 +160,11 @@ export const BentoGridItem = ({
           )}
           {id === 6 && (
             <div className="mt-5 relative">
-             
               <div
                 className={`absolute -bottom-5 right-0 ${
                   copied ? "block" : "block"
                 }`}
               >
-               
                 <Lottie options={defaultOptions} height={200} width={400} />
               </div>
 
